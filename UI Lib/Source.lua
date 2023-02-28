28412db8449668581bafe19444d54ff886a78dd6
rabbit <126437023+rabbitware@users.noreply.github.com>
2023-02-28 16:27:56 -0500

    Update Source.lua

--- UI Lib/Source.lua
+++ UI Lib/Source.lua
@@ -412,7 +412,7 @@ function Library:create(options)
 		Name = "rabbitware",
 		Size = UDim2.fromOffset(600, 400),
 		Theme = self.Themes[settings.Theme],
-		Link = "https://github.com/deeeity/mercury-lib"
+		Link = "https://github.com/rabbitware"
 	}, options)
 
 	if getgenv and getgenv().rabbitwareUI then
