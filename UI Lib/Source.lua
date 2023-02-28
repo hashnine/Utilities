ccc50d68b9e5c2e861c58456e8acb3e50e3554ae
rabbit <126437023+rabbitware@users.noreply.github.com>
2023-02-28 16:45:00 -0500

    Update Source.lua

--- UI Lib/Source.lua
+++ UI Lib/Source.lua
@@ -783,7 +783,8 @@ function Library:create(options)
 		Theme = {TextColor3 = {"WeakText", -20}},
 		TextScaled = true,
 		TextXAlignment = Enum.TextXAlignment.Left,
-		Text = tostring(os.date("%X")):sub(1, os.date("%X"):len()-3)
+		Text = "The date is " .. dt:FormatLocalTime("LL", "en-us")
+
 	})
 
 	do
