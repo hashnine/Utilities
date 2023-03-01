ca6ce6135cb747b0227193b2564966adf6ae0c95
rabbit <126437023+rabbitware@users.noreply.github.com>
2023-02-28 19:30:25 -0500

    Update src.lua

--- UI Lib/src.lua
+++ UI Lib/src.lua
@@ -666,7 +666,7 @@ function Library:create(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 5, 0.5, 0),
 		Size = UDim2.new(0, 15, 0, 15),
-		Image = "http://www.roblox.com/asset/?id=279461710",
+		Image = "http://www.roblox.com/asset/?id=4034483344",
 		Theme = {ImageColor3 = "StrongText"}
 	})
 
