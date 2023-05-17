9535239b628d97c3bcdbcb6d1b7d3934d2c84eda
splash <126437023+hashnine@users.noreply.github.com>
2023-05-16 22:45:00 -0400

    Update src.lua

--- UI Lib/src.lua
+++ UI Lib/src.lua
@@ -877,7 +877,7 @@ function Library:create(options)
 	settingsTab:keybind{
 		Name = "Toggle Key",
 		Description = "Key to show/hide the UI.",
-		Keybind = Enum.KeyCode.BackQuote,
+		Keybind = Enum.KeyCode.Backquote,
 		Callback = function()
 			self.Toggled = not self.Toggled
 			Library:show(self.Toggled)
